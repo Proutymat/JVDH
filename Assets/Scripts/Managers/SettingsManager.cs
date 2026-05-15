0102e4f28eb1ae4a01e3603cfcc762c7a762cf35
Matéo Avventuriero <77232541+Proutymat@users.noreply.github.com>
2026-05-15 17:00:11 +0200

    chore: add odin package

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -1,9 +1,8 @@
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
-using UnityEngine.Localization.Tables;
-using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
@@ -12,14 +11,22 @@ public class SettingsManager : MonoBehaviour
     private static SettingsManager m_instance;
     public static SettingsManager Instance => m_instance;
     
-    [Header("Set in Inspector")]
+    [Title("Set in Inspector")]
+    [Title("Main menu settings", horizontalLine: false)]
     [SerializeField] private Slider m_globalVolumeSlider;
     [SerializeField] private TMP_Text m_subtitlesText;
     [SerializeField] private TMP_Text m_languageText;
     [SerializeField] private TMP_Text m_windowedModeText;
     [SerializeField] private TMP_Text m_videoPlayerControlsText;
     [SerializeField] private VideoPlayerControls m_videoPlayerControlsScript;
-    
+    [Title("Pause menu settings", horizontalLine: false)]
+    [SerializeField] private Slider m_globalVolumeSliderInGame;
+    [SerializeField] private TMP_Text m_subtitlesTextInGame;
+    [SerializeField] private TMP_Text m_languageTextInGame;
+    [SerializeField] private TMP_Text m_windowedModeTextInGame;
+    [SerializeField] private TMP_Text m_videoPlayerControlsTextInGame;
+    [SerializeField] private VideoPlayerControls m_videoPlayerControlsScriptInGame;
+    [Title("Localization keys", horizontalLine: false)]
     [SerializeField] private LocalizedString  m_enabledTextKey;
     [SerializeField] private LocalizedString  m_disabledTextKey;
     [SerializeField] private LocalizedString  m_subtitlesTextKey;
@@ -64,14 +71,24 @@ public class SettingsManager : MonoBehaviour
 
     private void SetSubtitlesButtonsText()
     {
+        // Main menu
         m_subtitlesText.text = m_subtitlesTextKey.GetLocalizedString();
         m_subtitlesText.text += m_subtitles ? m_enabledTextKey.GetLocalizedString() : m_disabledTextKey.GetLocalizedString();
+        
+        // Pause menu
+        m_subtitlesTextInGame.text = m_subtitlesTextKey.GetLocalizedString();
+        m_subtitlesTextInGame.text += m_subtitles ? m_enabledTextKey.GetLocalizedString() : m_disabledTextKey.GetLocalizedString();
     }
 
     private void SetVPCButtonsText()
     {
+        // Main menu
         m_videoPlayerControlsText.text = m_videoPlayerControlsTextKey.GetLocalizedString();
         m_videoPlayerControlsText.text += m_videoPlayerControls ? m_enabledTextKey.GetLocalizedString() : m_disabledTextKey.GetLocalizedString();
+        
+        // Pause menu
+        m_videoPlayerControlsTextInGame.text = m_videoPlayerControlsTextKey.GetLocalizedString();
+        m_videoPlayerControlsTextInGame.text += m_videoPlayerControls ? m_enabledTextKey.GetLocalizedString() : m_disabledTextKey.GetLocalizedString();
     }
 
     public void VideoPlayerControlsButton()
@@ -89,8 +106,13 @@ public class SettingsManager : MonoBehaviour
 
     private void SetWindowedModeButtonsText()
     {
+        // Main menu
         m_windowedModeText.text = m_windowedModeTextKey.GetLocalizedString();
         m_windowedModeText.text += m_windowedMode ? m_enabledTextKey.GetLocalizedString() : m_disabledTextKey.GetLocalizedString();
+        
+        // Pause menu
+        m_windowedModeTextInGame.text = m_windowedModeTextKey.GetLocalizedString();
+        m_windowedModeTextInGame.text += m_windowedMode ? m_enabledTextKey.GetLocalizedString() : m_disabledTextKey.GetLocalizedString();
     }
 
     public void WindowedModeButton()
